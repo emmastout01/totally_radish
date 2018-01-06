8a76673a813ee3d0ad3dc81d3b5d9dea5b944711
Levy Kohout <levykohout1@Levy-Kohout.local>
2018-01-06 15:40:55 -0600

    working text parse

--- server/public/scripts/controllers/imagecapture.controller.js
+++ server/public/scripts/controllers/imagecapture.controller.js
@@ -3,17 +3,27 @@ myApp.controller('ImageCapture', ImageCapture);
 function ImageCapture(Upload,UserService,$http) {
 
     var image = this;
-    var API_KEY = '25f539a74f88957';
-    var API_URL = "https://vision.googleapis.com/v1/images:annotate?key="+API_KEY;
+    var API_KEY = '25f539a74f88957' //
+
+  //  var API_KEY = 'AIzaSyDnHEnyUqglQTflBIECVl5mZDks5M-xRLY';
+  //  var API_URL = "https://vision.googleapis.com/v1/images:annotate?key="+API_KEY;
     image.data = {};
     image.progressPercentage=0;
 
     image.uploadFile = function() {
-      var requestData = {
+
+    //  var fileReader = new FileReader();
+//fileReader.readAsDataURL(image.file);
+//fileReader.onload = function (e) {
+  //var dataUrl = e.target.result;
+  //var base64Data = dataUrl.substr(dataUrl.indexOf('base64,') + 'base64,'.length);
+
+
+  /*    var requestData = {
                 "requests": [
                     {
                       "image": {
-                      "content": image.file
+                      "content": base64Data
                     },
                     "features": [
                         {
@@ -23,27 +33,32 @@ function ImageCapture(Upload,UserService,$http) {
                     }
                   ]
                 }; //end of requestData
-      $http.post(API_URL,requestData).then(function(response){
-        console.log(response);
-      })
-      /*  Upload.upload({
-            url: 'https://vision.googleapis.com/v1/images:annotate',
-            data:
-        }).then(function(resp) {
-            image.data = resp.data.ParsedResults[0].ParsedText;
-            console.log(image.data);
-            // console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ', resp.data);
-            image.processData();
+    //  $http.post(API_URL,requestData).then(function(response){
+      //  console.log(response);
+    //  })*/
+       Upload.upload({
+         url: 'https://api.ocr.space/parse/image',
+          data: {
+              base64Image: image.file,
+              'apikey': API_KEY
+          }
+          //  url: API_URL,
+            //data:requestData
+        }).then(function(response) {
+          image.data = response.data.ParsedResults[0].ParsedText;
+         console.log(image.data);
+         // console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ', resp.data);
+         image.processData();
 
         }, function(resp) {
             console.log('Error status: ' + resp.status);
         }, function(evt) {
             image.progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
             // console.log('progress: ' + receipt.progressPercentage + '% ' + evt.config.data.file.name);
-        });*/
+        });
 
     }; //End of uploadFile function
-
+//};
     image.processData = function() {
 
 
