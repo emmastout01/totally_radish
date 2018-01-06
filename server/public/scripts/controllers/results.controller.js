6f805072813b0aee8d284ba76dd45ab79a678ea5
Emma Stout <emma.stout01@gmail.com>
2018-01-06 15:17:03 -0600

    centered text, changed aniation colors

--- server/public/scripts/controllers/results.controller.js
+++ server/public/scripts/controllers/results.controller.js
@@ -3,6 +3,8 @@ myApp.controller('ResultsController', function(UserService) {
     var vm = this;
     vm.userService = UserService;
     vm.isVegan = false;
+    vm.nonVeganIngredients = ['Caprylic Acid', 'Fish Liver Oil'];
+    vm.nonVeganDescriptions = ['A liquid fatty acid from cow’s or goat’s milk Also from palm, coconut, and other plant oils In perfumes, soaps Derivatives: Caprylic Triglyceride, Caprylamine Oxide, Capryl Betaine Alternatives: plant sources, especially coconut oil', 'Female hormones from pregnant mares’ urine Considered a drug Can have harmful systemic effects if used by children Used for reproductive problems and in birth control pills and Premarin, a menopausal drug In creams, perfumes, and lotions Has a negligible effect in the creams as a skin restorative; simple vegetable-source emollients are considered better Alternatives: oral contraceptives and menopausal drugs based on synthetic steroids or phytoestrogens from plants, especially palm-kernel oil Menopausal symptoms can also be treated with diet and herbs']
 
     // This function will show if the product is vegan or not. For now, until the backend routes are set up, this function is just set to toggle on the click of 'toggle results' button.
     vm.showResults = function() {
