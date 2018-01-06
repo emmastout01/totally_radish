3f241599f2387842533320b720440ce28bdb421b
Emma Stout <emma.stout01@gmail.com>
2018-01-06 17:39:09 -0600

    push before merging

--- server/public/scripts/services/nutrition.service.js
+++ server/public/scripts/services/nutrition.service.js
@@ -5,15 +5,15 @@ myApp.service('NutritionService', function($http, $location){
     self.ingredientList = [];
     
     self.getNutrition = function(response) {
-        $http.get('https://api.nutritionix.com/v1_1/search/' + self.productToSend + '?results=0:5&fields=item_name,brand_name,nf_ingredient_statement&appId=' + '' + '&appKey=' + '')
+        $http.get('https://api.nutritionix.com/v1_1/search/' + self.productToSend + '?results=0:5&fields=item_name,brand_name,nf_ingredient_statement&appId=' + process.env.APP_ID + '&appKey=' + process.env.APP_KEY)
     .then(function(response) {
         console.log('full response', response);
-        response.data.hits[4].fields.nf_ingredient_statement = self.ingredientList;
+        response.data.hits[0].fields.nf_ingredient_statement = self.ingredientList;
         console.log('ingredients', self.ingredientList);
     })
 }
 
-    self.getNutrition();
+    // self.getNutrition();
 
   });
   
