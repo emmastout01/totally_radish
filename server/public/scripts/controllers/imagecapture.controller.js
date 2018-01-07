03642036ffbb2f94c31936b451c9876ba121b78e
Emma Stout <emma.stout01@gmail.com>
2018-01-07 11:43:20 -0600

    style changes

--- server/public/scripts/controllers/imagecapture.controller.js
+++ server/public/scripts/controllers/imagecapture.controller.js
@@ -52,7 +52,7 @@ function ImageCapture(Upload,UserService,$http, NutritionService) {
 function move() {
     var elem = document.getElementById("myBar"); 
     var width = 1;
-    var id = setInterval(frame, 30);
+    var id = setInterval(frame, 100);
     function frame() {
         if (width >= 100) {
             clearInterval(id);
